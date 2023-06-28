bdce5d3d4b7686a7f141a9954216c10bfd188fd3
Ronald Gustavo <ronaald_gustavoo@hotmail.com>
2023-06-28 09:59:38 -0300

    Feat: calculo da média

--- exerciciosGuanabara/Program.cs
+++ exerciciosGuanabara/Program.cs
@@ -184,6 +184,21 @@ class Program
     }
     static void CalculoMedia()
     {
+        float nota1, nota2, nota3, nota4, media;
 
+        Console.WriteLine("Digite a primeira nota: ");
+        nota1 = float.Parse(Console.ReadLine());
+        
+        Console.WriteLine("Digite a segunda nota: ");
+        nota2 = float.Parse(Console.ReadLine());
+
+        Console.WriteLine("Digite a terceira nota: ");
+        nota3 = float.Parse(Console.ReadLine());
+
+        Console.WriteLine("Digite a quarta nota: ");
+        nota4 = float.Parse(Console.ReadLine());
+
+        media = (nota1 + nota2 + nota3 + nota4) / 4;
+        Console.WriteLine("A media do aluno foi " + media.ToString("F2"));
     }
 }
