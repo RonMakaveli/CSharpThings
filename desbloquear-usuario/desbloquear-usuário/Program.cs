23ab6d71987ebb554e8075ade2c1c3f2eff35f8f
Ronald Rodrigues Pereira <57762848+RonMakaveli@users.noreply.github.com>
2023-07-15 17:51:49 -0300

    Add files via upload

---
+++ desbloquear-usuario/desbloquear-usuário/Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.DirectoryServices;
+
+namespace DesbloqueioUsuariosAD
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string domainPath = "LDAP://DC=mydomain,DC=com"; // substitua pelo caminho do seu domínio
+
+            while (true)
+            {
+                Console.Write("Digite o nome de usuário a ser desbloqueado (ou digite 'sair' para encerrar o programa): ");
+                string username = Console.ReadLine();
+
+                if (username.ToLower() == "sair")
+                {
+                    break;
+                }
+
+                DirectoryEntry directoryEntry = new DirectoryEntry(domainPath);
+                DirectorySearcher searcher = new DirectorySearcher(directoryEntry);
+
+                searcher.Filter = $"(sAMAccountName={username})";
+                searcher.PropertiesToLoad.Add("lockoutTime");
+
+                SearchResult result = searcher.FindOne();
+
+                if (result != null)
+                {
+                    // obtém o objeto do usuário
+                    DirectoryEntry userEntry = result.GetDirectoryEntry();
+
+                    // verifica se o usuário está bloqueado
+                    if (userEntry.Properties["lockoutTime"].Value is long lockoutTime && lockoutTime != 0)
+                    {
+                        // modifica o atributo "lockoutTime"
+                        userEntry.Properties["lockoutTime"].Value = 0;
+
+                        // salva as alterações no Active Directory
+                        userEntry.CommitChanges();
+
+                        Console.WriteLine($"Usuário {username} desbloqueado com sucesso.");
+                    }
+                    else
+                    {
+                        Console.WriteLine($"Usuário {username} não está bloqueado.");
+                    }
+                }
+                else
+                {
+                    Console.WriteLine($"Usuário {username} não encontrado.");
+                }
+            }
+        }
+    }
+}
