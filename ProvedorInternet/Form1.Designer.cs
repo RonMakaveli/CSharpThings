29d6405076d060959780269f41671d623182865f
Ronald Rodrigues Pereira <57762848+RonMakaveli@users.noreply.github.com>
2023-09-05 21:45:59 -0300

    Calculadora de mensalidade
    Essa calculadora permite realizar o calculo para um provedor de Internet

--- ProvedorInternet/Form1.Designer.cs
+++ ProvedorInternet/Form1.Designer.cs
@@ -28,120 +28,130 @@
         /// </summary>
         private void InitializeComponent()
         {
-            listBox1 = new ListBox();
+            ListCidades = new ListBox();
             label1 = new Label();
-            panel1 = new Panel();
+            CaixaVelocidade = new Panel();
+            RbtSessenta = new RadioButton();
+            RbtTrinta = new RadioButton();
+            RbtDez = new RadioButton();
             label2 = new Label();
-            radioButton1 = new RadioButton();
-            radioButton2 = new RadioButton();
-            radioButton3 = new RadioButton();
-            checkBox1 = new CheckBox();
-            checkBox2 = new CheckBox();
+            OptPromo1 = new CheckBox();
+            OptPromo2 = new CheckBox();
             label3 = new Label();
             label4 = new Label();
-            panel1.SuspendLayout();
+            Calcular = new Button();
+            menuStrip1 = new MenuStrip();
+            arquivoToolStripMenuItem = new ToolStripMenuItem();
+            sairToolStripMenuItem = new ToolStripMenuItem();
+            ajudaToolStripMenuItem = new ToolStripMenuItem();
+            sobreToolStripMenuItem = new ToolStripMenuItem();
+            CaixaVelocidade.SuspendLayout();
+            menuStrip1.SuspendLayout();
             SuspendLayout();
             // 
-            // listBox1
+            // ListCidades
             // 
-            listBox1.FormattingEnabled = true;
-            listBox1.ItemHeight = 15;
-            listBox1.Items.AddRange(new object[] { "São paulo ", "Rio de janeiro ", "Campo Grande ", "Manaus ", "Roraima", "Rio Grande do Sul", "Rio Grande do Norte ", "Bahia ", "Espiro Santo", "Paraná", "Santa Catarina", "Acre" });
-            listBox1.Location = new Point(45, 31);
-            listBox1.Name = "listBox1";
-            listBox1.Size = new Size(120, 94);
-            listBox1.TabIndex = 0;
-            listBox1.SelectedIndexChanged += listBox1_SelectedIndexChanged;
+            ListCidades.FormattingEnabled = true;
+            ListCidades.ItemHeight = 15;
+            ListCidades.Items.AddRange(new object[] { "São paulo ", "Rio de janeiro ", "Campo Grande ", "Manaus ", "Roraima", "Rio Grande do Sul", "Rio Grande do Norte ", "Bahia ", "Espiro Santo", "Paraná", "Santa Catarina", "Acre" });
+            ListCidades.Location = new Point(57, 58);
+            ListCidades.Name = "ListCidades";
+            ListCidades.Size = new Size(120, 94);
+            ListCidades.TabIndex = 0;
+            ListCidades.SelectedIndexChanged += listBox1_SelectedIndexChanged;
             // 
             // label1
             // 
             label1.AutoSize = true;
             label1.Font = new Font("Segoe UI", 10F, FontStyle.Bold, GraphicsUnit.Point);
-            label1.Location = new Point(39, 9);
+            label1.Location = new Point(42, 38);
             label1.Name = "label1";
             label1.Size = new Size(154, 19);
             label1.TabIndex = 1;
             label1.Text = "Selecionar sua cidade";
             label1.Click += label1_Click;
             // 
-            // panel1
+            // CaixaVelocidade
             // 
-            panel1.Controls.Add(radioButton3);
-            panel1.Controls.Add(radioButton2);
-            panel1.Controls.Add(radioButton1);
-            panel1.Location = new Point(42, 172);
-            panel1.Name = "panel1";
-            panel1.Size = new Size(120, 100);
-            panel1.TabIndex = 2;
+            CaixaVelocidade.Controls.Add(RbtSessenta);
+            CaixaVelocidade.Controls.Add(RbtTrinta);
+            CaixaVelocidade.Controls.Add(RbtDez);
+            CaixaVelocidade.Location = new Point(56, 197);
+            CaixaVelocidade.Name = "CaixaVelocidade";
+            CaixaVelocidade.Size = new Size(120, 100);
+            CaixaVelocidade.TabIndex = 2;
+            // 
+            // RbtSessenta
+            // 
+            RbtSessenta.AutoSize = true;
+            RbtSessenta.Location = new Point(3, 78);
+            RbtSessenta.Name = "RbtSessenta";
+            RbtSessenta.Size = new Size(63, 19);
+            RbtSessenta.TabIndex = 2;
+            RbtSessenta.Text = "60 MBs";
+            RbtSessenta.UseVisualStyleBackColor = true;
+            RbtSessenta.CheckedChanged += radioButton3_CheckedChanged;
+            // 
+            // RbtTrinta
+            // 
+            RbtTrinta.AutoSize = true;
+            RbtTrinta.Location = new Point(3, 40);
+            RbtTrinta.Name = "RbtTrinta";
+            RbtTrinta.Size = new Size(63, 19);
+            RbtTrinta.TabIndex = 1;
+            RbtTrinta.Text = "30 MBs";
+            RbtTrinta.UseVisualStyleBackColor = true;
+            RbtTrinta.CheckedChanged += radioButton2_CheckedChanged;
+            // 
+            // RbtDez
+            // 
+            RbtDez.AutoSize = true;
+            RbtDez.Checked = true;
+            RbtDez.Location = new Point(3, 3);
+            RbtDez.Name = "RbtDez";
+            RbtDez.Size = new Size(66, 19);
+            RbtDez.TabIndex = 0;
+            RbtDez.TabStop = true;
+            RbtDez.Text = "10 MBs ";
+            RbtDez.UseVisualStyleBackColor = true;
+            RbtDez.CheckedChanged += radioButton1_CheckedChanged;
             // 
             // label2
             // 
             label2.AutoSize = true;
             label2.Font = new Font("Segoe UI", 10F, FontStyle.Bold, GraphicsUnit.Point);
-            label2.Location = new Point(1, 140);
+            label2.Location = new Point(12, 171);
             label2.Name = "label2";
             label2.Size = new Size(231, 19);
             label2.TabIndex = 3;
             label2.Text = "Selecione a velocidade desejada.";
             label2.Click += label2_Click;
             // 
-            // radioButton1
+            // OptPromo1
             // 
-            radioButton1.AutoSize = true;
-            radioButton1.Checked = true;
-            radioButton1.Location = new Point(3, 3);
-            radioButton1.Name = "radioButton1";
-            radioButton1.Size = new Size(94, 19);
-            radioButton1.TabIndex = 0;
-            radioButton1.TabStop = true;
-            radioButton1.Text = "radioButton1";
-            radioButton1.UseVisualStyleBackColor = true;
+            OptPromo1.AutoSize = true;
+            OptPromo1.Location = new Point(28, 350);
+            OptPromo1.Name = "OptPromo1";
+            OptPromo1.Size = new Size(162, 19);
+            OptPromo1.TabIndex = 4;
+            OptPromo1.Text = "Até três e-mail fornecidos";
+            OptPromo1.UseVisualStyleBackColor = true;
             // 
-            // radioButton2
+            // OptPromo2
             // 
-            radioButton2.AutoSize = true;
-            radioButton2.Location = new Point(3, 40);
-            radioButton2.Name = "radioButton2";
-            radioButton2.Size = new Size(94, 19);
-            radioButton2.TabIndex = 1;
-            radioButton2.Text = "radioButton2";
-            radioButton2.UseVisualStyleBackColor = true;
-            // 
-            // radioButton3
-            // 
-            radioButton3.AutoSize = true;
-            radioButton3.Location = new Point(3, 78);
-            radioButton3.Name = "radioButton3";
-            radioButton3.Size = new Size(94, 19);
-            radioButton3.TabIndex = 2;
-            radioButton3.Text = "radioButton3";
-            radioButton3.UseVisualStyleBackColor = true;
-            // 
-            // checkBox1
-            // 
-            checkBox1.AutoSize = true;
-            checkBox1.Location = new Point(25, 321);
-            checkBox1.Name = "checkBox1";
-            checkBox1.Size = new Size(162, 19);
-            checkBox1.TabIndex = 4;
-            checkBox1.Text = "Até três e-mail fornecidos";
-            checkBox1.UseVisualStyleBackColor = true;
-            // 
-            // checkBox2
-            // 
-            checkBox2.AutoSize = true;
-            checkBox2.Location = new Point(25, 346);
-            checkBox2.Name = "checkBox2";
-            checkBox2.Size = new Size(165, 19);
-            checkBox2.TabIndex = 5;
-            checkBox2.Text = "Página pessoal hospedada";
-            checkBox2.UseVisualStyleBackColor = true;
+            OptPromo2.AutoSize = true;
+            OptPromo2.Location = new Point(28, 375);
+            OptPromo2.Name = "OptPromo2";
+            OptPromo2.Size = new Size(165, 19);
+            OptPromo2.TabIndex = 5;
+            OptPromo2.Text = "Página pessoal hospedada";
+            OptPromo2.UseVisualStyleBackColor = true;
             // 
             // label3
             // 
             label3.AutoSize = true;
             label3.Font = new Font("Segoe UI", 10F, FontStyle.Bold, GraphicsUnit.Point);
-            label3.Location = new Point(39, 294);
+            label3.Location = new Point(42, 315);
             label3.Name = "label3";
             label3.Size = new Size(155, 19);
             label3.TabIndex = 6;
@@ -151,46 +161,107 @@
             // label4
             // 
             label4.AutoSize = true;
-            label4.Location = new Point(82, 400);
+            label4.Location = new Point(42, 479);
             label4.Name = "label4";
             label4.Size = new Size(62, 15);
             label4.TabIndex = 7;
             label4.Text = "Resultado:";
             label4.Click += label4_Click;
             // 
+            // Calcular
+            // 
+            Calcular.Font = new Font("Segoe UI", 10F, FontStyle.Bold, GraphicsUnit.Point);
+            Calcular.Location = new Point(68, 420);
+            Calcular.Margin = new Padding(2);
+            Calcular.Name = "Calcular";
+            Calcular.Size = new Size(97, 38);
+            Calcular.TabIndex = 8;
+            Calcular.Text = "Calcular";
+            Calcular.UseVisualStyleBackColor = true;
+            Calcular.Click += button1_Click;
+            // 
+            // menuStrip1
+            // 
+            menuStrip1.Items.AddRange(new ToolStripItem[] { arquivoToolStripMenuItem, ajudaToolStripMenuItem });
+            menuStrip1.Location = new Point(0, 0);
+            menuStrip1.Name = "menuStrip1";
+            menuStrip1.Size = new Size(248, 24);
+            menuStrip1.TabIndex = 9;
+            menuStrip1.Text = "menuStrip1";
+            // 
+            // arquivoToolStripMenuItem
+            // 
+            arquivoToolStripMenuItem.DropDownItems.AddRange(new ToolStripItem[] { sairToolStripMenuItem });
+            arquivoToolStripMenuItem.Name = "arquivoToolStripMenuItem";
+            arquivoToolStripMenuItem.Size = new Size(61, 20);
+            arquivoToolStripMenuItem.Text = "Arquivo";
+            // 
+            // sairToolStripMenuItem
+            // 
+            sairToolStripMenuItem.Name = "sairToolStripMenuItem";
+            sairToolStripMenuItem.Size = new Size(93, 22);
+            sairToolStripMenuItem.Text = "Sair";
+            sairToolStripMenuItem.Click += sairToolStripMenuItem_Click;
+            // 
+            // ajudaToolStripMenuItem
+            // 
+            ajudaToolStripMenuItem.DropDownItems.AddRange(new ToolStripItem[] { sobreToolStripMenuItem });
+            ajudaToolStripMenuItem.Name = "ajudaToolStripMenuItem";
+            ajudaToolStripMenuItem.Size = new Size(50, 20);
+            ajudaToolStripMenuItem.Text = "Ajuda";
+            // 
+            // sobreToolStripMenuItem
+            // 
+            sobreToolStripMenuItem.Name = "sobreToolStripMenuItem";
+            sobreToolStripMenuItem.Size = new Size(104, 22);
+            sobreToolStripMenuItem.Text = "Sobre";
+            sobreToolStripMenuItem.Click += sobreToolStripMenuItem_Click;
+            // 
             // Form1
             // 
             AutoScaleDimensions = new SizeF(7F, 15F);
             AutoScaleMode = AutoScaleMode.Font;
-            ClientSize = new Size(243, 441);
+            ClientSize = new Size(248, 525);
+            Controls.Add(Calcular);
             Controls.Add(label4);
             Controls.Add(label3);
-            Controls.Add(checkBox2);
-            Controls.Add(checkBox1);
+            Controls.Add(OptPromo2);
+            Controls.Add(OptPromo1);
             Controls.Add(label2);
-            Controls.Add(panel1);
+            Controls.Add(CaixaVelocidade);
             Controls.Add(label1);
-            Controls.Add(listBox1);
+            Controls.Add(ListCidades);
+            Controls.Add(menuStrip1);
             Name = "Form1";
+            StartPosition = FormStartPosition.CenterScreen;
             Text = "Calcular Mensalidade de Internet";
-            panel1.ResumeLayout(false);
-            panel1.PerformLayout();
+            Load += Form1_Load;
+            CaixaVelocidade.ResumeLayout(false);
+            CaixaVelocidade.PerformLayout();
+            menuStrip1.ResumeLayout(false);
+            menuStrip1.PerformLayout();
             ResumeLayout(false);
             PerformLayout();
         }
 
         #endregion
 
-        private ListBox listBox1;
+        private ListBox ListCidades;
         private Label label1;
-        private Panel panel1;
+        private Panel CaixaVelocidade;
         private Label label2;
-        private RadioButton radioButton3;
-        private RadioButton radioButton2;
-        private RadioButton radioButton1;
-        private CheckBox checkBox1;
-        private CheckBox checkBox2;
+        private RadioButton RbtSessenta;
+        private RadioButton RbtTrinta;
+        private RadioButton RbtDez;
+        private CheckBox OptPromo1;
+        private CheckBox OptPromo2;
         private Label label3;
         private Label label4;
+        private Button Calcular;
+        private MenuStrip menuStrip1;
+        private ToolStripMenuItem arquivoToolStripMenuItem;
+        private ToolStripMenuItem ajudaToolStripMenuItem;
+        private ToolStripMenuItem sobreToolStripMenuItem;
+        private ToolStripMenuItem sairToolStripMenuItem;
     }
 }
--- ProvedorInternet/Form1.cs
+++ ProvedorInternet/Form1.cs
@@ -1,3 +1,5 @@
+using System.Windows.Forms;
+
 namespace ProvedorInternet
 {
     public partial class Form1 : Form
@@ -35,6 +37,76 @@ namespace ProvedorInternet
         private void label4_Click(object sender, EventArgs e)
         {
 
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void radioButton1_CheckedChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void radioButton2_CheckedChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void radioButton3_CheckedChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (ListCidades.SelectedIndex == -1)
+            {
+                label4.Text = "Por favor, selecione uma cidade.";
+                return;
+            }
+
+            int cidadeSelecionada = ListCidades.SelectedIndex;
+
+            decimal valorVelocidade = 0.00M;
+            if (RbtDez.Checked)
+            {
+                valorVelocidade = 0.00M;
+            }
+            else if (RbtTrinta.Checked)
+            {
+                valorVelocidade = 30.00M;
+            }
+            else if (RbtSessenta.Checked)
+            {
+                valorVelocidade = 60.00M;
+            }
+
+            decimal valorOpcionais = 0.00M;
+            if (OptPromo1.Checked)
+            {
+                valorOpcionais += 10.00M;
+            }
+            if (OptPromo2.Checked)
+            {
+                valorOpcionais += 15.00M;
+            }
+
+            decimal[] valoresBase = { 140.00M, 140.00M, 135.00M, 120.00M, 120.00M, 120.00M, 120.00M, 120.00M, 120.00M, 120.00M, 120.00M, 120.00M };
+            decimal mensalidadeTotal = valoresBase[cidadeSelecionada] + valorVelocidade + valorOpcionais;
+
+            label4.Text = $"Mensalidade total: R${mensalidadeTotal:F2}";
+        }
+
+        private void sobreToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Ronald Gustavo - 36283631.", "Sobre");
+        }
+
+        private void sairToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
         }
     }
 }
