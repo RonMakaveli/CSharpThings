d1929b444ad4d415da7c46a7560d8fc848688672
Ronald Rodrigues Pereira <57762848+RonMakaveli@users.noreply.github.com>
2023-10-17 21:16:15 -0300

    Upload pasta principal

---
+++ WFGravarDados/WFGravarDados/Form1.Designer.cs
@@ -0,0 +1,178 @@
+﻿namespace WFGravarDados
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtNome = new System.Windows.Forms.TextBox();
+            this.txtTelefone = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtEmail = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.btnEnviar = new System.Windows.Forms.Button();
+            this.lstContatos = new System.Windows.Forms.ListView();
+            this.label4 = new System.Windows.Forms.Label();
+            this.txtBuscar = new System.Windows.Forms.TextBox();
+            this.btnBuscar = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.Font = new System.Drawing.Font("Arial Narrow", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(12, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(79, 23);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "NOME";
+            // 
+            // txtNome
+            // 
+            this.txtNome.Location = new System.Drawing.Point(16, 35);
+            this.txtNome.Name = "txtNome";
+            this.txtNome.Size = new System.Drawing.Size(283, 20);
+            this.txtNome.TabIndex = 1;
+            // 
+            // txtTelefone
+            // 
+            this.txtTelefone.Location = new System.Drawing.Point(16, 100);
+            this.txtTelefone.Name = "txtTelefone";
+            this.txtTelefone.Size = new System.Drawing.Size(283, 20);
+            this.txtTelefone.TabIndex = 3;
+            // 
+            // label2
+            // 
+            this.label2.Font = new System.Drawing.Font("Arial Narrow", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(12, 74);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(79, 23);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "TELEFONE";
+            // 
+            // txtEmail
+            // 
+            this.txtEmail.Location = new System.Drawing.Point(16, 173);
+            this.txtEmail.Name = "txtEmail";
+            this.txtEmail.Size = new System.Drawing.Size(283, 20);
+            this.txtEmail.TabIndex = 5;
+            // 
+            // label3
+            // 
+            this.label3.Font = new System.Drawing.Font("Arial Narrow", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(12, 147);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(79, 23);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "E-MAIL";
+            // 
+            // btnEnviar
+            // 
+            this.btnEnviar.BackColor = System.Drawing.Color.GreenYellow;
+            this.btnEnviar.Font = new System.Drawing.Font("Arial Narrow", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnEnviar.Location = new System.Drawing.Point(201, 220);
+            this.btnEnviar.Name = "btnEnviar";
+            this.btnEnviar.Size = new System.Drawing.Size(98, 45);
+            this.btnEnviar.TabIndex = 6;
+            this.btnEnviar.Text = "ENVIAR";
+            this.btnEnviar.UseVisualStyleBackColor = false;
+            // 
+            // lstContatos
+            // 
+            this.lstContatos.HideSelection = false;
+            this.lstContatos.Location = new System.Drawing.Point(321, 74);
+            this.lstContatos.Name = "lstContatos";
+            this.lstContatos.Size = new System.Drawing.Size(343, 119);
+            this.lstContatos.TabIndex = 7;
+            this.lstContatos.UseCompatibleStateImageBehavior = false;
+            // 
+            // label4
+            // 
+            this.label4.Font = new System.Drawing.Font("Arial Narrow", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(317, 9);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(137, 23);
+            this.label4.TabIndex = 8;
+            this.label4.Text = "BUSCAR CONTATO";
+            // 
+            // txtBuscar
+            // 
+            this.txtBuscar.Location = new System.Drawing.Point(321, 35);
+            this.txtBuscar.Name = "txtBuscar";
+            this.txtBuscar.Size = new System.Drawing.Size(248, 20);
+            this.txtBuscar.TabIndex = 9;
+            // 
+            // btnBuscar
+            // 
+            this.btnBuscar.BackColor = System.Drawing.Color.DodgerBlue;
+            this.btnBuscar.Font = new System.Drawing.Font("Arial Narrow", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnBuscar.Location = new System.Drawing.Point(575, 27);
+            this.btnBuscar.Name = "btnBuscar";
+            this.btnBuscar.Size = new System.Drawing.Size(91, 33);
+            this.btnBuscar.TabIndex = 10;
+            this.btnBuscar.Text = "BUSCAR";
+            this.btnBuscar.UseVisualStyleBackColor = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(676, 290);
+            this.Controls.Add(this.btnBuscar);
+            this.Controls.Add(this.txtBuscar);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.lstContatos);
+            this.Controls.Add(this.btnEnviar);
+            this.Controls.Add(this.txtEmail);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.txtTelefone);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.txtNome);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtNome;
+        private System.Windows.Forms.TextBox txtTelefone;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtEmail;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button btnEnviar;
+        private System.Windows.Forms.ListView lstContatos;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox txtBuscar;
+        private System.Windows.Forms.Button btnBuscar;
+    }
+}
+
---
+++ WFGravarDados/WFGravarDados/Form1.cs
@@ -0,0 +1,104 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using MySql.Data.MySqlClient;
+
+namespace WFGravarDados
+{
+    public partial class Form1 : Form
+    {
+        private MySqlConnection Conexao;
+        private string data_source = "datasource=localhost;username=root" + "database=db_agenda;password=12345;";
+        public Form1()
+        {
+            InitializeComponent();
+
+            lstContatos.View = View.Details;
+            lstContatos.AllowColumnReorder = true;
+            lstContatos.FullRowSelect = true;
+            lstContatos.GridLines = true;
+
+            //Definição das colunas no listView (Nome da coluna, tamanho e alinhamento)
+            lstContatos.Columns.Add("ID", 30, HorizontalAlignment.Left);
+            lstContatos.Columns.Add("NOME", 150, HorizontalAlignment.Left);
+            lstContatos.Columns.Add("E-MAIL", 150, HorizontalAlignment.Left);
+            lstContatos.Columns.Add("TELEFONE", 30, HorizontalAlignment.Left);
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnEnviar_click(object sender, EventArgs e)
+        {
+            try
+            {
+                //Criar conexão
+                Conexao = new MySqlConnection(data_source);
+                string sql = "INSERT INTO contatos(nome, email, telefone " + "VALUES" + 
+                    "('" + txtNome.Text + "', '" + txtEmail.Text + "', '" + txtTelefone.Text + "')";
+
+
+                //Executar comando Insert
+                MySqlCommand comando = new MySqlCommand(sql, Conexao);
+
+                Conexao.Open();
+                comando.ExecuteReader();
+                MessageBox.Show("Inclusão efetuada!");
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+            finally
+            {
+                Conexao.Close();
+            }
+        }
+
+        private void btnBuscar_click(object sender, EventArgs e)
+        {
+            try
+            {
+                string q = "'%" + txtBuscar.Text + "%'";
+                //Criar conexão
+                Conexao = new MySqlConnection(data_source);
+                string sql = "SELECT * " +
+                             "FROM contatos WHERE nome " +
+                             "LIKE " + q + "OR email LIKE " + q;
+
+                MySqlCommand comando = new MySqlCommand(sql, Conexao);
+                Conexao.Open();
+                MySqlDataReader reader = comando.ExecuteReader();
+                lstContatos.Items.Clear();
+                while (reader.Read())
+                {
+                    string[] linha =
+                    {
+                    reader.GetString(0),
+                    reader.GetString(1),
+                    reader.GetString(2),
+                    reader.GetString(3),
+                };
+                    var linha_listview = new ListViewItem(linha);
+                    lstContatos.Items.Add(linha_listview);
+                }
+            }
+            catch(Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+            finally
+            {
+                Conexao.Close(); 
+            }
+        }
+    }
+}
